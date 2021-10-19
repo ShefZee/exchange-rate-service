8e6c5f45d65fef0c093df20930089fd6e6857ae1
Shefeeque <shefzee07@gmail.com>
2021-10-19 18:42:38 +0400

    1

--- src/main/java/com/shefzee/exchangerate/job/UpdateExchangeRateJob.java
+++ src/main/java/com/shefzee/exchangerate/job/UpdateExchangeRateJob.java
@@ -30,46 +30,38 @@ public class UpdateExchangeRateJob {
     private boolean logEnabled;
     @Scheduled(cron = "${cron.time}")
     public void updateRates(){
-        List<ExchangeRate> rates =  exchangeRateRepository.findAll();
-        for(ExchangeRate a : rates){
-            //get Base exchange rate
-            BaseExchangeRate baseExchangeRate = baseExchangeRateRepository.findBySourceCurrencyAndTargetCurrency(a.getSourceCurrency(),a.getTargetCurrency());
-            double rate = a.getValue();
-            double decimal = getRandomDecimal(a.getValue(), a.getValue()+ 1) - a.getValue();
-            if(new Random().nextBoolean()){
-                rate = rate + decimal;
-            }else{
-                rate = rate - decimal;
-            }
-            if(Objects.nonNull(baseExchangeRate)){
-                if(rate < baseExchangeRate.getValue()-3){
+        try {
+            List<ExchangeRate> rates =  exchangeRateRepository.findAll();
+            for(ExchangeRate a : rates){
+                //get Base exchange rate
+                BaseExchangeRate baseExchangeRate = baseExchangeRateRepository.findBySourceCurrencyAndTargetCurrency(a.getSourceCurrency(),a.getTargetCurrency());
+                double rate = a.getValue();
+                double decimal = getRandomDecimal(a.getValue(), a.getValue()+ 1) - a.getValue();
+                if(new Random().nextBoolean()){
                     rate = rate + decimal;
-                }else if(rate > baseExchangeRate.getValue() + 3){
+                }else{
                     rate = rate - decimal;
                 }
-            }
-            if(rate < 0){
-                rate = rate * -1;
-            }
-            a.setValue(rate);
-            a =  exchangeRateRepository.save(a);
-            if (logEnabled) {
-                System.out.println(new Date() + " : " + a.getSourceCurrency() + " - " + a.getTargetCurrency() + " = " + a.getValue() );
-            }
-        }
-       /* exchangeRateRepository.findAll()
-                .forEach(a -> {
-                    double rate = a.getValue();
-                    double decimal = getRandomDecimal(a.getValue(), a.getValue()+ 1);
-                    if(new Random().nextBoolean()){
+                if(Objects.nonNull(baseExchangeRate)){
+                    if(rate < baseExchangeRate.getValue()-3){
                         rate = rate + decimal;
-                    }else{
+                    }else if(rate > baseExchangeRate.getValue() + 3){
                         rate = rate - decimal;
                     }
-                    a.setValue(rate);
-                    a =  exchangeRateRepository.save(a);
+                }
+                if(rate < 0){
+                    rate = rate * -1;
+                }
+                a.setValue(rate);
+                a =  exchangeRateRepository.save(a);
+                if (logEnabled) {
                     System.out.println(new Date() + " : " + a.getSourceCurrency() + " - " + a.getTargetCurrency() + " = " + a.getValue() );
-                });*/
+                }
+            }
+        } catch (Exception e) {
+
+        }
+
     }
 
     double getRandomDecimal(double min, double max) {
